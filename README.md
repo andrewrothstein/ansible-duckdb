e74b74ce0d469c5d2af5e249be8be143d9b3089f
Andrew Rothstein <andrew.rothstein@gmail.com>
2023-08-23 21:08:09 -0400

    duckdb:0.8.1

---
+++ README.md
@@ -0,0 +1,40 @@
+andrewrothstein.duckdb
+=========
+
+![Build Status](https://github.com/andrewrothstein/ansible-duckdb/actions/workflows/build.yml/badge.svg)
+
+A brief description of the andrewrothstein.duckdb goes here.
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.duckdb
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+MIRROR=https://github.com/duckdb/duckdb/releases/download
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-zip}
+    local platform="${os}-${arch}"
+    # https://github.com/duckdb/duckdb/releases/download/v0.8.1/duckdb_cli-linux-amd64.zip
+    local url="${MIRROR}/v${ver}/duckdb_cli-${platform}.${archive_type}"
+    local lfile="${DIR}/duckdb_cli-${ver}-${platform}.${archive_type}"
+
+    if [ ! -e $lfile ];
+    then
+        curl -sSLf -o $lfile $url
+    fi
+
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dlver () {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dl $ver linux aarch64
+    dl $ver linux amd64
+    dl $ver linux i386
+    dl $ver linux rpi
+    dl $ver osx universal
+    dl $ver windows amd64
+    dl $ver windows i386
+}
+
+dlver ${1:-0.8.1}
